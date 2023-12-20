b4f7fb19a365f3cdeb74c95c71beb6a8978055cd
Sebastián <sebastian@MacBook-Air-de-Sebastian.local>
2023-12-20 10:33:42 +0100

    Upload Testing

--- Sprint 1/Proj.IntelliJ/Sprint 1/Tests/S1/Testing/Nivel_1/Ejercicio2/CalculoDniTest.java
+++ Sprint 1/Proj.IntelliJ/Sprint 1/Tests/S1/Testing/Nivel_1/Ejercicio2/CalculoDniTest.java
@@ -1,40 +1,49 @@
 package S1.Testing.Nivel_1.Ejercicio2;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-public class CalculoDniTest {
-    @Test
-    public void testLetrasyNumerosDni()
-    {
-        /**
-         *                                                  Tabla DNI´s
-         * RESTO 	0 	1 	2 	3 	4 	5 	6 	7 	8 	9 	10 	11  12 	13 	14 	15 	16 	17 	18 	19 	20 	21 	22
-         * LETRA 	T 	R 	W 	A 	G 	M 	Y 	F 	P 	D 	X 	B   N 	J 	Z 	S 	Q 	V 	H 	L 	C 	K 	E
-         */
-        System.out.println("::: Test - 'Dado un numero específico de DNI, corroborar su letra' :::");
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class CalculoDniTest
+{
+    /**
+     *                                                  Tabla DNI´s
+     * RESTO 	0 	1 	2 	3 	4 	5 	6 	7 	8 	9 	10 	11  12 	13 	14 	15 	16 	17 	18 	19 	20 	21 	22
+     * LETRA 	T 	R 	W 	A 	G 	M 	Y 	F 	P 	D 	X 	B   N 	J 	Z 	S 	Q 	V 	H 	L 	C 	K 	E
+     */
+
+    @ParameterizedTest
+    @MethodSource (value = "getData") // Nombre del metodo que contiene los parámetros para ejecutar el test
+    @DisplayName(value = "::: Test - 'Dado un numero específico de DNI, corroborar su letra' :::")
+    public void testLetrasyNumerosDni(char letraEsperada, int dniTest)
+    {
         CalculoDni calculadorDni = new CalculoDni();
-        char[] letrasEsperadas = new char[]{'T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q',
-                    'V','H', 'L','C','K','E'};
-        int numero0 = 2345023;  // Letra 'N'
-        int numero1 = 12345023; // Letra 'A'
-        int numero2 = 22345023; // Letra 'V'
-        int numero3 = 32345023; // Letra 'P'
-        int numero4 = 42345023; // Letra 'E'
-        int numero5 = 52345023; // Letra 'J'
-        int numero6 = 62345023; // Letra 'G'
-        int numero7 = 72345023; // Letra 'H'
-        int numero8 = 82345023; // Letra 'D'
-        int numero9 = 80345023; // Letra 'C'
-        assertEquals(letrasEsperadas[12],calculadorDni.obtenerLetraDni(numero0));
-        assertEquals(letrasEsperadas[3],calculadorDni.obtenerLetraDni(numero1));
-        assertEquals(letrasEsperadas[17],calculadorDni.obtenerLetraDni(numero2));
-        assertEquals(letrasEsperadas[8],calculadorDni.obtenerLetraDni(numero3));
-        assertEquals(letrasEsperadas[22],calculadorDni.obtenerLetraDni(numero4));
-        assertEquals(letrasEsperadas[13],calculadorDni.obtenerLetraDni(numero5));
-        assertEquals(letrasEsperadas[4],calculadorDni.obtenerLetraDni(numero6));
-        assertEquals(letrasEsperadas[18],calculadorDni.obtenerLetraDni(numero7));
-        assertEquals(letrasEsperadas[9],calculadorDni.obtenerLetraDni(numero8));
-        assertEquals(letrasEsperadas[20],calculadorDni.obtenerLetraDni(numero9));
+        assertEquals(letraEsperada,calculadorDni.obtenerLetraDni(dniTest));
+    }
+
+    /**
+     * JUnit ya sabe que tiene que tomar el par de valores de cada "Object[]" contenidos en la "List<Object[]> ret" y
+     * pasarlos, en el mismo orden en que se encuentran almacenados, como argumentos de la funcion
+     * "testLetrasyNumerosDni". Esto se hará de manera iterativa con todos los elementos de la lista nombrada.
+     */
+    public static Iterable<Object[]> getData()
+    {
+        List<Object[]> ret = new ArrayList<>();
+        ret.add(new Object[] {'N',2345023} );
+        ret.add(new Object[]{'A',12345023});
+        ret.add(new Object[]{'V',22345023});
+        ret.add(new Object[]{'P',32345023});
+        ret.add(new Object[]{'E',42345023});
+        ret.add(new Object[]{'J',52345023});
+        ret.add(new Object[]{'G',62345023});
+        ret.add(new Object[]{'H',72345023});
+        ret.add(new Object[]{'D',82345023});
+        ret.add(new Object[]{'C',80345023});
+        return ret;
     }
 }
 
