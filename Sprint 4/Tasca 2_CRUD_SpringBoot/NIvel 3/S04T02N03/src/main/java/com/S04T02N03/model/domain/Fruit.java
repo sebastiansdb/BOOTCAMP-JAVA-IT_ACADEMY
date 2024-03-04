9d51fecf5c9e869f408da7fb30cbd090764b4cdc
sebastian <sdebianchetti@gmail.com>
2024-03-04 13:53:06 +0100

    04/03/24

--- Sprint 4/Tasca 2_CRUD_SpringBoot/NIvel 3/S04T02N03/src/main/java/com/S04T02N03/model/domain/Fruit.java
+++ Sprint 4/Tasca 2_CRUD_SpringBoot/NIvel 3/S04T02N03/src/main/java/com/S04T02N03/model/domain/Fruit.java
@@ -6,15 +6,6 @@ import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-/*
-– @Entity annotation indicates that the class is a persistent Java class.
-– @Table annotation provides the table that maps this entity.
-– @Id annotation is for the primary key.
-– @GeneratedValue annotation is used to define generation strategy for the primary key. GenerationType.AUTO means
-  Auto Increment field.
-– @Column annotation is used to define the column in database that maps annotated field.
- */
-
 @Document(collection = "fruits")
 public class Fruit {
 
